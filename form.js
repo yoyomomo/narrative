e31a34759e661b59e47fafbfba5b358d9019f647
cesca-codes <delacann@sheridancollege.ca>
2021-03-25 03:19:39 -0400

    chapter 1
    more chunks

--- form.js
+++ form.js
@@ -1,7 +1,13 @@
 var answer = document.getElementById('guess-input').name;
-
 var hint = document.getElementById('guess-input').value;
 var a = document.getElementById('lockChap2');
+var answer2 = document.getElementById('guess-input2').name;
+var hint2 = document.getElementById('guess-input2').value;
+var answer3 = document.getElementById('guess-input3').name;
+var hint3 = document.getElementById('guess-input3').value;
+
+
+
 
 function guessAnswer() {
     $("button.guess-submit").click(function (event) {
@@ -34,3 +40,70 @@ function enterSubmit() {
 }
 
 enterSubmit();
+
+
+function guessAnswer2() {
+    $("button.guess-submit2").click(function (event) {
+
+        var guess2 = $('#guess-input2').val();
+        guess2 = guess2.toLowerCase();
+
+        if (guess2 == answer2) {
+
+            document.getElementById('sign-in').style.display = "block";
+            window.location = "#sign-in";
+
+
+        } else {
+            $('#wrong2').show().fadeOut(1000);
+            $('#guess-input2').val(hint);
+        }
+
+    });
+
+}
+
+function enterSubmit2() {
+    $("#guess-input2").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#guess-submit2").click();
+        }
+    });
+
+    guessAnswer2();
+}
+
+enterSubmit2();
+
+function guessAnswer3() {
+    $("button.guess-submit3").click(function (event) {
+
+        var guess3 = $('#guess-input3').val();
+        guess3 = guess3.toLowerCase();
+
+        if (guess3 == answer3) {
+
+            document.getElementById('sign-in2').style.display = "block";
+            window.location = "#sign-in2";
+
+
+        } else {
+            $('#wrong3').show().fadeOut(1000);
+            $('#guess-input3').val(hint);
+        }
+
+    });
+
+}
+
+function enterSubmit3() {
+    $("#guess-input3").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#guess-submit3").click();
+        }
+    });
+
+    guessAnswer3();
+}
+
+enterSubmit3();
