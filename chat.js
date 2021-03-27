421f62b401587026315dbcac92da37552f3e8157
cesca-codes <delacann@sheridancollege.ca>
2021-03-27 15:22:40 -0400

    Chat

--- chat.js
+++ chat.js
@@ -1,1 +1,12 @@
+// Cody by GBra 4.669 (Stackoverflow User) *** START ***
 
+const myHTMLCollection = document.getElementsByClassName("invisible");
+const HTMLElementsArr = [...myHTMLCollection];
+
+function showMessage() {
+    if (HTMLElementsArr.length > 0) {
+        HTMLElementsArr.shift().classList.remove('invisible');
+    }
+}
+
+// Cody by GBra 4.669 (Stackoverflow User) *** END ***
