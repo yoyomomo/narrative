1464bf4915245ea71f0ad0e630fd90636f48eca3
cesca-codes <delacann@sheridancollege.ca>
2021-03-27 17:25:15 -0400

    chat js

--- chat.js
+++ chat.js
@@ -1,12 +1,52 @@
-// Cody by GBra 4.669 (Stackoverflow User) *** START ***
+// Code by GBra 4.669 (https://stackoverflow.com/questions/66834196/how-to-make-multiple-divs-appear-one-after-the-other-using-javascript/66834457?noredirect=1#comment118141166_66834457) *** START ***
 
 const myHTMLCollection = document.getElementsByClassName("invisible");
 const HTMLElementsArr = [...myHTMLCollection];
 
-function showMessage() {
+function blueMessage() {
     if (HTMLElementsArr.length > 0) {
         HTMLElementsArr.shift().classList.remove('invisible');
+
+    }
+
+    if (!HTMLElementsArr.length) {
+        greyMessage();
+
     }
 }
 
-// Cody by GBra 4.669 (Stackoverflow User) *** END ***
+// Code by GBra 4.669 (https://stackoverflow.com/questions/66834196/how-to-make-multiple-divs-appear-one-after-the-other-using-javascript/66834457?noredirect=1#comment118141166_66834457) *** END ***
+
+
+function greyMessage() {
+    setTimeout(show_second, 1000);
+}
+
+function show_second() {
+    document.getElementById("msg4").style.display = "block";
+    setTimeout(show_third, 1000);
+}
+
+function show_third() {
+    document.getElementById("msg5").style.display = "block";
+    setTimeout(show_fourth, 1000);
+}
+
+function show_fourth() {
+    document.getElementById("msg6").style.display = "block";
+    setTimeout(show_fifth, 1000);
+}
+
+function show_fifth() {
+    document.getElementById("msg7").style.display = "block";
+    setTimeout(show_sixth, 1000);
+}
+
+function show_sixth() {
+    document.getElementById("msg8").style.display = "block";
+    setTimeout(show_seventh, 1000);
+}
+
+function show_seventh() {
+    document.getElementById("msg9").style.display = "block";
+}
