6d5a12e7f4f1f4b8713846ca76eef95823846523
cesca-codes <delacann@sheridancollege.ca>
2021-03-27 22:59:09 -0400

    chat sound

--- chat.js
+++ chat.js
@@ -8,7 +8,8 @@ var msg6 = document.getElementById("msg6");
 var msg7 = document.getElementById("msg7");
 var msg8 = document.getElementById("msg8");
 var msg9 = document.getElementById("msg9");
-var chatInput = [
+var audio = new Audio('facebookSound.mp3');
+var chatInputArray = [
    'Her name is Sam...',
    'Did you know her or h...'
   ];
@@ -20,50 +21,64 @@ var chatInput = [
 function blueMessage() {
     if (HTMLElementsArr.length > 0) {
         HTMLElementsArr.shift().classList.remove('invisible');
-        $(".chatInput").text(chatInput[1]);
+        $(".chatInput").text(chatInputArray[1]);
 
     }
 
     if (HTMLElementsArr.length > 1) {
-        $(".chatInput").text(chatInput[0]);
+        $(".chatInput").text(chatInputArray[0]);
 
     }
+
 
     if (!HTMLElementsArr.length) {
         greyMessage();
+        document.getElementById('chatBox').style.display = "none";
+
 
     }
+
 }
 
 // Code by GBra 4.669 (https://stackoverflow.com/questions/66834196/how-to-make-multiple-divs-appear-one-after-the-other-using-javascript/66834457?noredirect=1#comment118141166_66834457) *** END ***
 
+function remove(chatInput) {
+    var element = chatInput;
+    element.remove();
+}
+
 function greyMessage() {
     setTimeout(show_msg4, 1500);
+
 
 }
 
 function show_msg4() {
     document.getElementById("msg4").style.display = "block";
     setTimeout(show_msg5, 1500);
+    audio.play(); //Fix This
+
+
 
 }
 
+
 function show_msg5() {
     document.getElementById("msg5").style.display = "block";
-    setTimeout(show_msg6, 3000);
+    setTimeout(show_msg6, 2500);
     msg5.scrollIntoView({
         behavior: 'smooth'
     });
+
 
 }
 
 function show_msg6() {
     document.getElementById("msg6").style.display = "block";
-    setTimeout(show_msg7, 3000);
+    setTimeout(show_msg7, 2500);
     msg6.scrollIntoView({
         behavior: 'smooth'
     });
-
 
 }
 
@@ -81,6 +96,7 @@ function show_msg8() {
     msg8.scrollIntoView({
         behavior: 'smooth'
     });
+
 }
 
 function show_msg9() {
