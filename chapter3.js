7b74da7e4ebbf8583b06578522d21751a9ce03ee
cesca-codes <delacann@sheridancollege.ca>
2021-04-05 00:58:34 -0400

    fonts, chapter 3

---
+++ chapter3.js
@@ -0,0 +1,37 @@
+var answer6 = document.getElementById('guess-input6').name;
+
+//     FOURTH QUESTION START
+function guessAnswer6() {
+    $("button.guess-submit6").click(function (event) {
+
+        var guess6 = $('#guess-input6').val();
+        guess6 = guess6.toLowerCase();
+
+        if (guess6 == answer6) {
+            document.getElementById('ending').style.display = "block";
+            $('html, body').animate({
+                scrollTop: $("#ending").offset().top
+            }, 2000);
+
+
+        } else {
+            $('#wrong6').show().fadeOut(1000);
+        }
+
+    });
+
+}
+
+
+function enterSubmit6() {
+    $("#guess-input6").keyup(function (event) {
+        if (event.keyCode == 13) {
+            $("#guess-submit6").click();
+        }
+    });
+
+    guessAnswer6();
+}
+
+enterSubmit6();
+//     FOURTH QUESTION END
